5de928827c38041fd6fa0c68fc720a00801e887d
Georgi Popov <gwvr028@gmail.com>
2024-02-07 21:04:29 +0200

    Initial

---
+++ afaf/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <h1>AFAF</h1>
+    </BrowserRouter>
+  )   
+}
+
+export default App
