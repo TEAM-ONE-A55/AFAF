cb154ca9efda0012abf28e6da3f0ffa86eeb7686
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-02-08 13:31:23 +0200

    Adding basic components

---
+++ afaf/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header () {
+    return (
+        <div>
+            <p>Logo</p>
+            <p>Input</p>
+            <p>Login</p>
+        </div>
+    )
+}
---
+++ afaf/src/components/Navi/Navi.jsx
@@ -0,0 +1,7 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navi () {
+    return (
+        <NavLink to="">All threads</NavLink>
+    )
+}
