9074456433c86035f2df5dfc8d9c3b00f99b8fa7
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-02-10 23:53:42 +0200

    Implementing Login function

--- afaf/src/views/Login/Login.jsx
+++ afaf/src/views/Login/Login.jsx
@@ -3,13 +3,32 @@ import { loginUser } from "../../services/auth.service";
 import { NavLink } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (e) => {
+    e.preventDefault();
+    loginUser(email, password).catch((error) => {
+      console.log(error);
+    });
+  };
   return (
     <div>
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder="Email" /> <br />
-        <input type="password" placeholder="Password" /> <br />
-        <button onClick={loginUser}>Login</button> No account yet?
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />{" "}
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />{" "}
+        <br />
+        <button onClick={login}>Login</button> No account yet?
         <NavLink to="/register">Register now</NavLink>
       </form>
     </div>
