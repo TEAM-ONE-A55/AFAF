dce97b89527aa25890d6f64b31767f8b881854a8
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-02-10 22:30:07 +0200

    Implementing Login functionality

--- afaf/src/views/Login/Login.jsx
+++ afaf/src/views/Login/Login.jsx
@@ -1,8 +1,17 @@
-export default function Login () {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+import { useState } from "react";
+import { loginUser } from "../../services/auth.service";
+import { NavLink } from "react-router-dom";
 
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Email" /> <br />
+        <input type="password" placeholder="Password" /> <br />
+        <button onClick={loginUser}>Login</button> No account yet?
+        <NavLink to="/register">Register now</NavLink>
+      </form>
+    </div>
+  );
 }
