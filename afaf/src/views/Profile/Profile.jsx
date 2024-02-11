8341e4d775edfd0dc3aed7944c71ff60cad8af27
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-02-11 04:45:47 +0200

    Add Logout button in Profile.jsx

--- afaf/src/views/Profile/Profile.jsx
+++ afaf/src/views/Profile/Profile.jsx
@@ -1,7 +1,21 @@
-export default function Profile () {
-    return (
-        <div>
-            <h3>Profile</h3>
-        </div>
-    )
+import { useContext } from "react";
+import Button from "../../components/Button/Button";
+import { AppContext } from "../../context/AppContext";
+import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../../services/auth.service";
+
+export default function Profile() {
+  const { setContext } = useContext(AppContext);
+  const navigate = useNavigate();
+  const logout = () => {
+    logoutUser();
+    setContext({ user: null, userData: null });
+    navigate("/");
+  };
+  return (
+    <div>
+      <h3>Profile</h3>
+      <Button onClick={logout}>Logout</Button>
+    </div>
+  );
 }
