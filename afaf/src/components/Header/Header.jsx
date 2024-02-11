3c08dcf4ba7eb7effbfe14c7f6014134b8bfa0fc
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-02-11 04:08:48 +0200

    Add condition in Header

--- afaf/src/components/Header/Header.jsx
+++ afaf/src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
 
 export default function Header() {
+  const { user, userData } = useContext(AppContext);
+  const navigate = useNavigate();
+
   return (
-    <div>
-      {/* <p>Logo</p> */}
-      {/* <p>Input</p>
-            <p>Login</p> */}
-      <nav>
-        <span>Logo</span>
-        <input type="text" />
+    <header>
+      <span onClick={() => navigate("/")}>Logo</span>
+      <input type="text" />
+      {user ? (
+        <>
+          <NavLink to="/profile">Profile</NavLink>
+          <h4>Welcome, {userData && userData.handle}!</h4>
+        </>
+      ) : (
         <NavLink to="/login">Login</NavLink>
-      </nav>
-    </div>
+      )}
+    </header>
   );
 }
