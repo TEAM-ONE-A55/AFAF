ef6a304470573b0fb22b243bfe1db8336615b7fb
Georgi Popov <gwvr028@gmail.com>
2024-02-15 17:29:27 +0200

    Subtle changes added to Button

--- afaf/src/components/Button/Button.jsx
+++ afaf/src/components/Button/Button.jsx
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
+import "./Button.css";
 
 export default function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
+  return <button style={{ fontFamily: "Lexend"}} onClick={onClick}>{children}</button>;
 }
 
 Button.propTypes = {
