d524ea871b9aceb10fc493a2bb311db9be6063f3
Georgi Popov <gwvr028@gmail.com>
2024-02-16 23:37:47 +0200

    Replace magic numbers, cleanup

--- afaf/src/constants/constants.js
+++ afaf/src/constants/constants.js
@@ -1,1 +1,9 @@
-export const defaultAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6tPLpqzJTFzBgrnKZwyVwlSnBeVsxdCV_iTuP_E4HYFVA8wezZWLTggAWZq9cIqyAElE&usqp=CAU"
+export const defaultAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6tPLpqzJTFzBgrnKZwyVwlSnBeVsxdCV_iTuP_E4HYFVA8wezZWLTggAWZq9cIqyAElE&usqp=CAU";
+
+export const MIN_TITLE_LENGTH = 16;
+
+export const MAX_TITLE_LENGTH = 64;
+
+export const MIN_CONTENT_LENGTH = 32;
+
+export const MAX_CONTENT_LENGTH = 8192;
