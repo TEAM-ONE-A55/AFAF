74998b7e6546eb0f8889021d9eaf2d6957b2f9e3
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-02-17 20:36:07 +0200

    Complete Admin Dashboard

--- afaf/src/views/Admin/Admin.jsx
+++ afaf/src/views/Admin/Admin.jsx
@@ -1,19 +1,22 @@
 import { useContext, useEffect, useState } from "react";
 import { getAllUsers, getUserByHandle } from "../../services/users.service";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Admin.css";
-import { getAllTopics } from "../../services/threads.service";
+import { getAllTopics, getTopicById } from "../../services/threads.service";
 import { blockUser, changeRole } from "../../functions/admin-functions";
 import { deleteUser } from "../../services/users.service";
 import toast from "react-hot-toast";
 import { AppContext } from "../../context/AppContext";
+import { deleteThread } from "../../functions/threads-functions";
 
 export function Admin() {
   const { userData } = useContext(AppContext);
   const [users, setUsers] = useState([]);
   const [topics, setTopics] = useState([]);
   const [user, setUser] = useState({});
+  const [topic, setTopic] = useState({});
   const navigate = useNavigate();
+  const [data, setData] = useState(false)
 
   useEffect(() => {
     getAllUsers()
@@ -25,12 +28,18 @@ export function Admin() {
     getAllTopics().then((topicData) => setTopics(topicData));
   }, []);
 
-  useEffect(() => {}, [user]);
+  useEffect(() => { }, [user]);
+
+  useEffect(() => { }, [topic]);
 
   const getUser = async (handle) => {
     const user = await getUserByHandle(handle);
     const userData = user.val();
     setUser(userData);
+  };
+
+  const getTopic = async (id) => {
+    getTopicById(id).then(setTopic);
   };
 
   const removeUser = async (user) => {
@@ -42,6 +51,16 @@ export function Admin() {
     });
   };
 
+  const handleDeleteThread = (topic) => {
+    getTopic(topic.id);
+    deleteThread(topic.author, topic.id, topic.uuid, topic.url);
+    toast.promise(window.location.reload(), {
+      loading: "Saving...",
+      success: <b>{`Topic ${topic.title} has been successfully deleted`}</b>,
+      error: <b>Could not delete this user.</b>,
+    });
+  };
+
   return (
     <div className="admin-container">
       {userData && userData.role === "admin" ? (
@@ -49,111 +68,144 @@ export function Admin() {
           <h3>Dashboard</h3>
           <hr />
           <br />
-          <h4>Total users</h4>
+          <h3>Total threads: {topics.length}</h3>
+          <h3>Total users: {users.length}</h3>
+          <br />
 
-          <table className="table">
-            <thead>
-              <tr>
-                <th>User</th>
-                <th>Name</th>
-                <th>Role</th>
-                <th>Date of registration</th>
-                <th>Total threads</th>
-                <th>Status</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => {
-                return (
-                  <tr key={user.handle}>
-                    <td>{user.handle}</td>
-                    <td>{user.name}</td>
-                    <td>
-                      {user.role === "admin" ? (
-                        <span style={{ color: "pink" }}>{user.role}</span>
-                      ) : (
-                        user.role
-                      )}
-                    </td>
-                    <td>{new Date(user.createdOn).toLocaleDateString()}</td>
-                    <td>{user.createdTopics && Object.keys(user.createdTopics).length || 0}</td>
-                    <td>
-                      {user.blocked === true ? (
-                        <span style={{ color: "red" }}>Blocked</span>
-                      ) : (
-                        "Active"
-                      )}
-                    </td>
-                    <td>
-                      <button
-                        onClick={() => navigate(`/profile/${user.handle}`)}
-                      >
-                        See Profile
-                      </button>
-                      <button
-                        onClick={() => {
-                          getUser(user.handle);
-                          user.handle && removeUser(user.handle);
-                        }}
-                      >
-                        Delete
-                      </button>
-                      <button
-                        onClick={() => {
-                          getUser(user.handle);
-                          blockUser(user, setUser);
-                        }}
-                      >
-                        Ban
-                      </button>
-                      <button
-                        onClick={() => {
-                          getUser(user.handle);
-                          changeRole(user, setUser);
-                        }}
-                      >
-                        Change Role
-                      </button>
-                    </td>
+          <h3 className="admin-data" onClick={() => setData(!data)}>{data ? "See all users" : "See all threads"}</h3>
+          {data ? (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>User</th>
+                  <th>Name</th>
+                  <th>Role</th>
+                  <th>Date of registration</th>
+                  <th>Total threads</th>
+                  <th>Status</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => {
+                  return (
+                    <tr key={user.handle}>
+                      <td>{user.handle}</td>
+                      <td>{user.name}</td>
+                      <td>
+                        {user.role === "admin" ? (
+                          <span style={{ color: "pink" }}>{user.role}</span>
+                        ) : (
+                          user.role
+                        )}
+                      </td>
+                      <td>{new Date(user.createdOn).toLocaleDateString()}</td>
+                      <td>
+                        {(user.createdTopics &&
+                          Object.keys(user.createdTopics).length) ||
+                          0}
+                      </td>
+                      <td>
+                        {user.blocked === true ? (
+                          <span style={{ color: "red" }}>Blocked</span>
+                        ) : (
+                          "Active"
+                        )}
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => navigate(`/profile/${user.handle}`)}
+                        >
+                          See Profile
+                        </button>
+                        <button
+                          onClick={() => {
+                            getUser(user.handle);
+                            user.handle && removeUser(user.handle);
+                          }}
+                        >
+                          Delete
+                        </button>
+                        <button
+                          onClick={() => {
+                            getUser(user.handle);
+                            blockUser(user, setUser);
+                          }}
+                        >
+                          Ban
+                        </button>
+                        <button
+                          onClick={() => {
+                            getUser(user.handle);
+                            changeRole(user, setUser);
+                          }}
+                        >
+                          Change Role
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          ) : (
+            <>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Published on</th>
+                    <th>Total likes</th>
+                    <th>Total comments</th>
+                    <th>Actions</th>
                   </tr>
-                );
-              })}
-            </tbody>
-          </table>
+                </thead>
+                <tbody>
+                  {topics
+                    .sort((a, b) => new Date(b.createdOn) - new Date(a.createdOn))
+                    .map((topic) => (
+                      <tr key={topic.id}>
+                        <td
+                          id="table-thread-title"
+                          onClick={() => navigate(`/single-thread/${topic.id}`)}
+                        >
+                          {topic.title}
+                        </td>
+                        <td>
+                          <Link to={`/profile/${topic.author}`}>
+                            {topic.author}
+                          </Link>
+                        </td>
+                        <td>{new Date(topic.createdOn).toLocaleString()}</td>
+                        <td>{Object.keys(topic.likedBy).length}</td>
+                        <td>{Object.keys(topic.commentedBy).length}</td>
+                        <td>
+                          <button
+                            onClick={() => navigate(`/single-thread/${topic.id}`)}
+                          >
+                            See Thread
+                          </button>
+                          <button
+                            onClick={() => {
+                              handleDeleteThread(topic);
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </>
+          )
+          }
 
-          <h4>Total threads</h4>
-
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Published on</th>
-                <th>Total likes</th>
-                <th>Total comments</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {topics
-                .sort((a, b) => new Date(b.createdOn) - new Date(a.createdOn))
-                .map((topic) => (
-                  <tr key={topic.id}>
-                    <td id="table-thread-title" onClick={() => navigate(`/single-thread/${topic.id}`)}>
-                      {topic.title}
-                    </td>
-                    <td>{topic.author}</td>
-                    <td>{new Date(topic.createdOn).toLocaleString()}</td>
-                    <td>{Object.keys(topic.likedBy).length}</td>
-                    <td>{Object.keys(topic.commentedBy).length}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
         </>
+
       ) : (
-        navigate('*')
+        navigate("*")
       )}
     </div>
   );
