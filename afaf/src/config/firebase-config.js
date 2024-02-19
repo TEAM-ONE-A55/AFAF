ff2334c1c1c5e997203691a26ed6aaa9e4c1addd
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-02-19 15:00:25 +0200

    Restore previous firebase config for testing

--- afaf/src/config/firebase-config.js
+++ afaf/src/config/firebase-config.js
@@ -3,6 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import {getStorage} from 'firebase/storage'
 
+// Old project config:
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCRTeDJ3btstvzD2WrlMJZU8aEdPshNtB8",
+//   authDomain: "afaf-fa771.firebaseapp.com",
+//   databaseURL: "https://afaf-fa771-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "afaf-fa771",
+//   storageBucket: "afaf-fa771.appspot.com",
+//   messagingSenderId: "483910820722",
+//   appId: "1:483910820722:web:97b279222c361435b573eb"
+// };
+
 const firebaseConfig = {
     apiKey: "AIzaSyD93AVpV6GnHj1xMZ-n85zAu-9RXjwST_E",
     authDomain: "afaf-backup.firebaseapp.com",
