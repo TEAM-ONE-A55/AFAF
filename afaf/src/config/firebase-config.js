fa159c507de53f9be1e9642f5c3b51c426f8b6d2
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-02-19 12:49:25 +0200

    Add a new firebase config data

--- afaf/src/config/firebase-config.js
+++ afaf/src/config/firebase-config.js
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRTeDJ3btstvzD2WrlMJZU8aEdPshNtB8",
-    authDomain: "afaf-fa771.firebaseapp.com",
-    projectId: "afaf-fa771",
-    storageBucket: "afaf-fa771.appspot.com",
-    messagingSenderId: "483910820722",
-    appId: "1:483910820722:web:97b279222c361435b573eb",
-    databaseURL: "https://afaf-fa771-default-rtdb.europe-west1.firebasedatabase.app/"
-};
+    apiKey: "AIzaSyD93AVpV6GnHj1xMZ-n85zAu-9RXjwST_E",
+    authDomain: "afaf-backup.firebaseapp.com",
+    databaseURL: "https://afaf-backup-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "afaf-backup",
+    storageBucket: "afaf-backup.appspot.com",
+    messagingSenderId: "182405219893",
+    appId: "1:182405219893:web:64ed2db3130f670156d37c",
+  };
 
 export const app = initializeApp(firebaseConfig);
 
